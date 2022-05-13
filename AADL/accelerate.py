5340ef202b6770bece460fbc009c9ef562f2b9d1
Massimiliano Lupo Pasini <7ml@ornl.gov>
2022-05-12 22:53:13 -0400

    anderson_reduced added

--- AADL/accelerate.py
+++ AADL/accelerate.py
@@ -40,6 +40,8 @@ def accelerated_step(self, closure=None):
                     acc_param = anderson.anderson_qr_factorization(X, self.acc_relaxation, self.acc_reg)
                 elif self.acc_type == 'anderson_normal_equation':
                     acc_param = anderson.anderson_normal_equation(X, self.acc_relaxation, self.acc_reg)
+                elif self.acc_type == "anderson_reduced":
+                    acc_param = anderson.anderson_qr_factorization_reduced(X, self.acc_relaxation, self.acc_reg)
 
                 # loss after non-accelerated optimizer
                 if closure is not None:
