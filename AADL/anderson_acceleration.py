c0ccebd0e6a5ea9cc00573a9b546c23ff5b2f7a5
allaffa <massimiliano.lupo.pasini@gmail.com>
2022-05-24 15:46:20 +0000

    random sketching added

--- AADL/anderson_acceleration.py
+++ AADL/anderson_acceleration.py
@@ -1,4 +1,6 @@
 import torch
+import time
+import random
 
 def anderson_qr_factorization(X, relaxation=1.0, regularization = 0.0):
     # Anderson Acceleration
@@ -14,13 +16,19 @@ def anderson_qr_factorization(X, relaxation=1.0, regularization = 0.0):
 
     if regularization == 0.0:
        # solve unconstrained least-squares problem
+       start = time.time()
        gamma = torch.linalg.lstsq(DR, DX[:, -1]).solution
+       finish = time.time()
     else:
        # solve augmented least-squares for Tykhonov regularization
        rhs = DX[:,-1]
        expanded_rhs    = torch.cat( (rhs, torch.zeros(DR.size(1))) )
        expanded_matrix = torch.cat( (DR, torch.sqrt(torch.tensor(regularization)) * torch.eye(DR.size(1))) )
+       start = time.perf_counter()
        gamma = torch.linalg.lstsq(expanded_matrix, expanded_rhs).solution
+       finish = time.perf_counter()
+
+    #print("Time: ", finish-start)
 
     # compute acceleration
     extr = X[:,-2] + DX[:,-1] - (DX[:,:-1]+DR)@gamma
@@ -36,13 +44,18 @@ def anderson_qr_factorization(X, relaxation=1.0, regularization = 0.0):
 
     return extr
 
-def anderson_qr_factorization_reduced(X, relaxation=1.0, regularization = 0.0, reduction_type="maximum", reduction_ratio=0.1):
+def anderson_qr_factorization_reduced(X, relaxation=1.0, regularization = 0.0, reduction_type="random"):
     # Anderson Acceleration
     # Take a matrix X of iterates such that X[:,i] = g(X[:,i-1])
     # Return acceleration for X[:,-1]
 
     assert X.ndim==2, "X must be a matrix"
     assert regularization >= 0.0, "regularization for least-squares must be >=0.0"
+
+    init_ratio = 0.01
+    max_ratio = 0.20
+
+    reduction_ratio = init_ratio
 
     # Compute residuals
     DX =  X[:,1:] -  X[:,:-1] # DX[:,i] =  X[:,i+1] -  X[:,i]
@@ -53,10 +66,42 @@ def anderson_qr_factorization_reduced(X, relaxation=1.0, regularization = 0.0, r
        if reduction_type == "maximum":
           num_entries_kept = int(DX.shape[0] * reduction_ratio)
           indices = torch.topk(DX[:, -1], num_entries_kept).indices
-          gamma = torch.linalg.lstsq(DR[indices, :], DX[indices, -1]).solution
+          restricted_residual = torch.zeros(DX.shape[0],)
+          restricted_residual[indices] = DX[indices, -1]
+          while torch.norm(DX[:, -1]-restricted_residual) > (1/200*(torch.norm(DX[:, -1])**2) * 1e-8):
+              reduction_ratio = reduction_ratio * 2
+              if reduction_ratio > max_ratio:
+                 reduction_ratio = max_ratio
+                 break
+              num_entries_kept = int(DX.shape[0] * reduction_ratio)
+              indices = torch.topk(DX[:, -1], num_entries_kept).indices
+              restricted_residual = torch.zeros(DX.shape[0],)
+              restricted_residual[indices] = DX[indices, -1]
+       elif reduction_type == "random":
+          num_entries_kept = int(DX.shape[0] * reduction_ratio)
+          start = time.perf_counter()
+          indices = random.sample(range(0, int(DX.shape[0])), num_entries_kept)
+          finish = time.perf_counter()
+          restricted_residual = torch.zeros(DX.shape[0],)
+          restricted_residual[indices] = DX[indices, -1]
+          while torch.norm(DX[:, -1]-restricted_residual) > (1/200*(torch.norm(DX[:, -1])**2) * 1e-8):
+              reduction_ratio = reduction_ratio * 2
+              if reduction_ratio > max_ratio:
+                 reduction_ratio = max_ratio
+                 break
+              num_entries_kept = int(DX.shape[0] * reduction_ratio)
+              indices = random.sample(range(0, int(DX.shape[0])), num_entries_kept)
+              finish = time.perf_counter()
+              restricted_residual = torch.zeros(DX.shape[0],)
+              restricted_residual[indices] = DX[indices, -1]
+          print("Time for generation of random numbers: ", finish-start)
        else:
           raise ValueError("reduction type NOT recognized: ")
 
+       start = time.perf_counter()
+       gamma = torch.linalg.lstsq(DR[indices, :], DX[indices, -1]).solution
+       finish = time.perf_counter()
+       print("Time to solve the least-squares: ", finish - start)
     else:
        # solve augmented least-squares for Tykhonov regularization
        rhs = DX[:,-1]
